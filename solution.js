ec04cd2c7478066b220bbcfbae3511a93e30fd1a
mora-of-crims <moraofcrims@gmail.com>
2026-06-14 14:52:30 +0500

    Create solution.js

---
+++ solution.js
@@ -0,0 +1,17 @@
+// TODO: Реализуйте функцию filterEven
+// Принимает массив чисел, возвращает только чётные числа
+export function filterEven(arr) {
+  throw new Error('Not implemented');
+}
+
+// TODO: Реализуйте функцию doubleAll
+// Принимает массив чисел, возвращает массив с каждым числом × 2
+export function doubleAll(arr) {
+  throw new Error('Not implemented');
+}
+
+// TODO: Реализуйте функцию sum
+// Принимает массив чисел, возвращает их сумму
+export function sum(arr) {
+  throw new Error('Not implemented');
+}
